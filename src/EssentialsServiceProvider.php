2eded35785e4b5bba76b396522fd41ba66695dac
Michał Pietrucha <mpietrucha50@gmail.com>
2026-03-23 12:48:08 +0100

    wip

--- src/EssentialsServiceProvider.php
+++ src/EssentialsServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Mpietrucha\Laravel\Essentials;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Support\Arr;
+use Mpietrucha\Laravel\Essentials\Auth\CachedEloquentUserProvider;
 use Mpietrucha\Laravel\Essentials\Commands\GenerateIdeHelpers;
 use Mpietrucha\Laravel\Essentials\Commands\GenerateMixinAnalyzers;
 use Mpietrucha\Laravel\Essentials\Commands\Lint;
@@ -21,5 +25,17 @@ class EssentialsServiceProvider extends ServiceProvider
             GenerateIdeHelpers::class,
             GenerateMixinAnalyzers::class,
         ]);
+    }
+
+    public function bootingPackage(): void
+    {
+        auth()->provider('cached', static function (Application $application, array $config): CachedEloquentUserProvider {
+            /** @var Hasher $hasher */
+            $hasher = $application->get('hash');
+
+            $model = Arr::string($config, 'model');
+
+            return new CachedEloquentUserProvider($hasher, $model);
+        });
     }
 }
